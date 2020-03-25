3aa3e8f6a8276f07f76a5f1693dc35cd6c86a00f
coolhot <53906558+cool-hot@users.noreply.github.com>
2020-03-25 19:22:36 +0900

    ProjectMethod

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,12 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1));
+//		Email.send("egoing@a.com", "Math floor", Math.floor(1.1));
+		
+	}
+
+}
---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WhyMethod {
+	
+public static void main(String[] args) throws IOException {
+		
+		System.out.println(twoTimes("a", "-"));
+		FileWriter fw = new FileWriter("out.txt");
+		fw.write(twoTimes("a", "*"));
+		fw.close();
+//		Email.send("egoing@a.com","two times a", twoTimes("a","&"));
+		
+    }	
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
+	}
+	
+							
+}
